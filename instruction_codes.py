170c20b58de8a311fc45cbf6d6f34bc42a59ffbb
Marwan Magdy <engmarwan232@gmail.com>
2023-01-14 14:43:38 +0200

    ROM writer : added the fetch instruction and solve RAM problem in logism

--- instruction_codes.py
+++ instruction_codes.py
@@ -1,10 +1,13 @@
-ROM = """v2.0 raw
-55 66 77 78
-"""
-for idx, word in enumerate(ROM.split()):
-    if "v2.0" not in word and "raw" not in word:
-        print(hex(idx-2),"->", word)
+from controll_constants import *
+# initialize ROM
+ROM = ["0" for _ in range(0x2000)]
+
+# Fetch instruction
+for i in range(0x3ff+1):
+    ROM[i] = st(R_OUT | IP_SEL | S2 | S1)
 
 with open("ROM", "w") as file: # Writing to the file
+    ROM.insert(0,"v2.0")
+    ROM.insert(1,"raw\n")
     file.write("")
-    file.write(ROM)
+    file.write(" ".join(ROM))
