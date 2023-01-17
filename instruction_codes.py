adf9669103e65128ac9868c4602f340903c9741e
Marwan Magdy <engmarwan232@gmail.com>
2023-01-17 10:43:02 +0200

    Adding full working looping instructions like JMP JC JZ and some other codes

--- instruction_codes.py
+++ instruction_codes.py
@@ -6,20 +6,65 @@ ROM = ["0" for _ in range(0x2000)]
 for i in range(0x3ff+1):
     ROM[i] = st(R_OUT | IP_SEL | S2 | S1)
 
-ROM[0x400] = st(R_OUT | IP_SEL | IP_COUNT | A_IN | IR_RESET)   # LDA #imediate
-ROM[0x401] = st(R_OUT | IP_SEL | IP_COUNT | A_IN | IR_RESET)   # LDA #imediate
-ROM[0x402] = st(R_OUT | IP_SEL | IP_COUNT | A_IN | IR_RESET)   # LDA #imediate
-ROM[0x403] = st(R_OUT | IP_SEL | IP_COUNT | A_IN | IR_RESET)   # LDA #imediate
+"""
+The first three bits are the micro_instruction counter 
+second 8 bits are the instruction it self
+the last 2 bits are flage register output from the last ALU operation
+"""
+ROM[0b001_00000000_00] = st(S2 | S1 | R_OUT | IP_SEL | IP_COUNT | A_IN | IR_RESET)   # LDA #imediate
+ROM[0b001_00000000_01] = st(S2 | S1 | R_OUT | IP_SEL | IP_COUNT | A_IN | IR_RESET)   # LDA #imediate
+ROM[0b001_00000000_10] = st(S2 | S1 | R_OUT | IP_SEL | IP_COUNT | A_IN | IR_RESET)   # LDA #imediate
+ROM[0b001_00000000_11] = st(S2 | S1 | R_OUT | IP_SEL | IP_COUNT | A_IN | IR_RESET)   # LDA #imediate
 
-ROM[0x400+4] = st(R_OUT | IP_SEL | IP_COUNT | X_IN | IR_RESET) # LDX #imediate
-ROM[0x401+4] = st(R_OUT | IP_SEL | IP_COUNT | X_IN | IR_RESET) # LDX #imediate
-ROM[0x402+4] = st(R_OUT | IP_SEL | IP_COUNT | X_IN | IR_RESET) # LDX #imediate
-ROM[0x403+4] = st(R_OUT | IP_SEL | IP_COUNT | X_IN | IR_RESET) # LDX #imediate
+ROM[0b001_00000001_00] = st(S2 | S1 | R_OUT | IP_SEL | IP_COUNT | X_IN | IR_RESET)   # LDX #imediate
+ROM[0b001_00000001_01] = st(S2 | S1 | R_OUT | IP_SEL | IP_COUNT | X_IN | IR_RESET)   # LDX #imediate
+ROM[0b001_00000001_10] = st(S2 | S1 | R_OUT | IP_SEL | IP_COUNT | X_IN | IR_RESET)   # LDX #imediate
+ROM[0b001_00000001_11] = st(S2 | S1 | R_OUT | IP_SEL | IP_COUNT | X_IN | IR_RESET)   # LDX #imediate
 
-ROM[0x400+8] = st(R_OUT | IP_SEL | IP_COUNT | Y_IN | IR_RESET) # LDY #imediate
-ROM[0x401+8] = st(R_OUT | IP_SEL | IP_COUNT | Y_IN | IR_RESET) # LDY #imediate
-ROM[0x402+8] = st(R_OUT | IP_SEL | IP_COUNT | Y_IN | IR_RESET) # LDY #imediate
-ROM[0x403+8] = st(R_OUT | IP_SEL | IP_COUNT | Y_IN | IR_RESET) # LDY #imediate
+ROM[0b001_00000010_00] = st(S2 | S1 | R_OUT | IP_SEL | IP_COUNT | Y_IN | IR_RESET)   # LDY #imediate
+ROM[0b001_00000010_01] = st(S2 | S1 | R_OUT | IP_SEL | IP_COUNT | Y_IN | IR_RESET)   # LDY #imediate
+ROM[0b001_00000010_10] = st(S2 | S1 | R_OUT | IP_SEL | IP_COUNT | Y_IN | IR_RESET)   # LDY #imediate
+ROM[0b001_00000010_11] = st(S2 | S1 | R_OUT | IP_SEL | IP_COUNT | Y_IN | IR_RESET)   # LDY #imediate
+
+ROM[0b001_00000011_00] = st(IR_RESET | FLAGS_SEL | S2 | S1 | X_OUT | ALU_TO_A | A_IN)   # ADDX #imediate
+ROM[0b001_00000011_01] = st(IR_RESET | FLAGS_SEL | S2 | S1 | X_OUT | ALU_TO_A | A_IN)   # ADDX #imediate
+ROM[0b001_00000011_10] = st(IR_RESET | FLAGS_SEL | S2 | S1 | X_OUT | ALU_TO_A | A_IN)   # ADDX #imediate
+ROM[0b001_00000011_11] = st(IR_RESET | FLAGS_SEL | S2 | S1 | X_OUT | ALU_TO_A | A_IN)   # ADDX #imediate
+
+ROM[0b001_00000100_00] = st(IR_RESET | FLAGS_SEL | S2 | S1 | X_OUT | ALU_TO_A | A_IN | CPL_A | ALU_OUT_CPL)   # SUBX #imediate
+ROM[0b001_00000100_01] = st(IR_RESET | FLAGS_SEL | S2 | S1 | X_OUT | ALU_TO_A | A_IN | CPL_A | ALU_OUT_CPL)   # SUBX #imediate
+ROM[0b001_00000100_10] = st(IR_RESET | FLAGS_SEL | S2 | S1 | X_OUT | ALU_TO_A | A_IN | CPL_A | ALU_OUT_CPL)   # SUBX #imediate
+ROM[0b001_00000100_11] = st(IR_RESET | FLAGS_SEL | S2 | S1 | X_OUT | ALU_TO_A | A_IN | CPL_A | ALU_OUT_CPL)   # SUBX #imediate
+
+ROM[0b001_00000101_00] = st(IR_RESET | FLAGS_SEL | S2 | S1 | Y_OUT | ALU_TO_A | A_IN)   # ADDY #imediate
+ROM[0b001_00000101_01] = st(IR_RESET | FLAGS_SEL | S2 | S1 | Y_OUT | ALU_TO_A | A_IN)   # ADDY #imediate
+ROM[0b001_00000101_10] = st(IR_RESET | FLAGS_SEL | S2 | S1 | Y_OUT | ALU_TO_A | A_IN)   # ADDY #imediate
+ROM[0b001_00000101_11] = st(IR_RESET | FLAGS_SEL | S2 | S1 | Y_OUT | ALU_TO_A | A_IN)   # ADDY #imediate
+
+ROM[0b001_00000110_00] = st(IR_RESET | FLAGS_SEL | S2 | S1 | Y_OUT | ALU_TO_A | A_IN | CPL_A | ALU_OUT_CPL)   # SUBY #imediate
+ROM[0b001_00000110_01] = st(IR_RESET | FLAGS_SEL | S2 | S1 | Y_OUT | ALU_TO_A | A_IN | CPL_A | ALU_OUT_CPL)   # SUBY #imediate
+ROM[0b001_00000110_10] = st(IR_RESET | FLAGS_SEL | S2 | S1 | Y_OUT | ALU_TO_A | A_IN | CPL_A | ALU_OUT_CPL)   # SUBY #imediate
+ROM[0b001_00000110_11] = st(IR_RESET | FLAGS_SEL | S2 | S1 | Y_OUT | ALU_TO_A | A_IN | CPL_A | ALU_OUT_CPL)   # SUBY #imediate
+
+ROM[0b001_00000111_00] = st(IR_RESET  | S1 | S2)   # NOP #imediate
+ROM[0b001_00000111_01] = st(IR_RESET  | S1 | S2)   # NOP #imediate
+ROM[0b001_00000111_10] = st(IR_RESET  | S1 | S2)   # NOP #imediate
+ROM[0b001_00000111_11] = st(IR_RESET  | S1 | S2)   # NOP #imediate
+
+ROM[0b001_00001000_00] = st(S2 | S1 | IP_SEL | IP_INP | IR_RESET | R_OUT)   # JMP #imediate
+ROM[0b001_00001000_01] = st(S2 | S1 | IP_SEL | IP_INP | IR_RESET | R_OUT)   # JMP #imediate
+ROM[0b001_00001000_10] = st(S2 | S1 | IP_SEL | IP_INP | IR_RESET | R_OUT)   # JMP #imediate
+ROM[0b001_00001000_11] = st(S2 | S1 | IP_SEL | IP_INP | IR_RESET | R_OUT)   # JMP #imediate
+
+ROM[0b001_00001001_00] = st(IR_RESET  | S1 | S2)   # NOP
+ROM[0b001_00001001_01] = st(S2 | S1 | IP_SEL | IP_INP | IR_RESET | R_OUT)   # Jz #imediate
+ROM[0b001_00001001_10] = st(IR_RESET  | S1 | S2)   # NOP
+ROM[0b001_00001001_11] = st(S2 | S1 | IP_SEL | IP_INP | IR_RESET | R_OUT)   # Jz #imediate
+
+ROM[0b001_00001001_00] = st(IR_RESET  | S1 | S2)   # NOP
+ROM[0b001_00001001_01] = st(IR_RESET  | S1 | S2)   # NOP
+ROM[0b001_00001001_10] = st(S2 | S1 | IP_SEL | IP_INP | IR_RESET | R_OUT)   # JC #imediate
+ROM[0b001_00001001_11] = st(S2 | S1 | IP_SEL | IP_INP | IR_RESET | R_OUT)   # JC #imediate
 
 with open("ROM", "w") as file: # Writing to the file
     ROM.insert(0,"v2.0")
